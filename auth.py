3b156c8b0b9c0dfc48e8ec8948e66c6ad41354a8
Callum Smith <callum@smither.org>
2017-03-12 22:45:25 +0000

    Updated auth.py
    auth.py now uses new indexes in the database

--- auth.py
+++ auth.py
@@ -9,22 +9,22 @@ def authenhandler(req):
     pw = req.get_basic_auth_pw()
     user = req.user
     path = req.hostname+req.uri
-    conn = mysql.connector.connect(database='webAuth', user='<USERNAME>', password='<PASSWORD>', charset='utf8mb4')
+    conn = mysql.connector.connect(database='webAuth', user='apache2', password='passwd', charset='utf8mb4')
     db=conn.cursor()
     query = """
-    SELECT allowed FROM (
-        SELECT path,allowed FROM UserAccess where user=%s AND %s LIKE CONCAT( path, '%' )
-        UNION ALL
-        SELECT path,allowed FROM UserGroup, GroupAccess where user=%s AND UserGroup.group=GroupAccess.group AND %s LIKE CONCAT( path, '%' )
-    ) as Access ORDER BY LENGTH(Access.path) DESC limit 1;
+        SELECT allowed FROM (
+            (SELECT priority,allowed FROM UserAccess where user=%s AND %s LIKE CONCAT( path, '%' ) ORDER BY priority DESC limit 1)
+            UNION
+            (SELECT priority,allowed FROM UserGroup, GroupAccess where user=%s AND UserGroup.group=GroupAccess.group AND %s LIKE CONCAT( path, '%' ) ORDER BY priority DESC limit 1)
+        ) as Access ORDER BY priority DESC limit 1;
     """
     db.execute(query,(user,path,user,path))
     access=db.fetchone()
-    if access==None:
-        return apache.HTTP_UNAUTHORIZED
     db.close()
     conn.close()
-    if access[0]==1:
+    if access==None:
+        return apache.HTTP_UNAUTHORIZED
+    elif access[0]==1:
        return apache.OK
     else:
        return apache.HTTP_UNAUTHORIZED
